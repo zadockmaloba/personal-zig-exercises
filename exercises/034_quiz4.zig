d80ed6356acc0eed792763f77c07799d14e53dc1
Zadock Maloba <malobazadock@gmail.com>
2024-05-31 17:40:27 +0300

    Quiz 4

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -9,10 +9,12 @@ const std = @import("std");
 
 const NumError = error{IllegalNumber};
 
-pub fn main() void {
+pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    const my_num: u32 = getNumber();
+    const my_num: u32 = getNumber() catch |err| {
+        return err;
+    };
 
     try stdout.print("my_num={}\n", .{my_num});
 }
